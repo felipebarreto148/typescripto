daa63dc0aaf00dfb172b06f6fc6619dc09a135c7
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-14 18:40:27 -0300

    feat: first types example

---
+++ tipos/tipos.js
@@ -0,0 +1,14 @@
+"use strict";
+// string
+let nome = 'Jõao';
+console.log(nome);
+// nome = 28
+// number
+let idade = 27;
+// idade = 'Ana'
+idade = 27.5;
+console.log(idade);
+// boolean
+let possuiHobbies = false;
+// possuiHobbies = 1
+console.log(possuiHobbies);
---
+++ tipos/tipos.ts
@@ -0,0 +1,15 @@
+// string
+let nome = 'Jõao'
+console.log(nome);
+// nome = 28
+
+// number
+let idade = 27;
+// idade = 'Ana'
+idade = 27.5;
+console.log(idade);
+
+// boolean
+let possuiHobbies = false;
+// possuiHobbies = 1
+console.log(possuiHobbies);
