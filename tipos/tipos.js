b2dde9386146b62811c3ad30d06bcb3555b240bc
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-14 18:47:56 -0300

    feat: Arrays and tuplas

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -1,6 +1,6 @@
 "use strict";
 // string
-let nome = 'Jõao';
+let nome = "Jõao";
 console.log(nome);
 // nome = 28
 // number
@@ -17,3 +17,14 @@ let minhaIdade;
 minhaIdade = 21;
 console.log(typeof minhaIdade);
 // minhaIdade = 'idade é 27';
+// array
+let hobbies = ["Cozinhar", "Praticar Esportes"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100, 200, 300];
+// hobbies = 100
+console.log(hobbies);
+// Tuplas
+let endereco = ["Av Principal", 99, ""];
+console.log(endereco);
+endereco = ["Rua importante", 1260, "Bloco C"];
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,5 +1,5 @@
 // string
-let nome: string = 'Jõao'
+let nome: string = "Jõao";
 console.log(nome);
 // nome = 28
 
@@ -19,3 +19,19 @@ let minhaIdade: number;
 minhaIdade = 21;
 console.log(typeof minhaIdade);
 // minhaIdade = 'idade é 27';
+
+// array
+let hobbies: any[] = ["Cozinhar", "Praticar Esportes"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+
+hobbies = [100, 200, 300];
+// hobbies = 100
+
+console.log(hobbies);
+
+// Tuplas
+let endereco: [string, number, string] = ["Av Principal", 99, ""];
+console.log(endereco);
+
+endereco = ["Rua importante", 1260, "Bloco C"]
