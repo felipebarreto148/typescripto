0c66095d07d01ad90289fcd44e2512374de94879
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-23 19:38:37 -0300

    feat: Change local of namespace

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,19 +1,6 @@
 "use strict";
-var Geometria;
-(function (Geometria) {
-    let Area;
-    (function (Area) {
-        const PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRect.ts"/>
 const PI = 2.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
