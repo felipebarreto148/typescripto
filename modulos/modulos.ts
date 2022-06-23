749a6ff80dd7f31118626ff50b091cbca1328045
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-23 20:05:03 -0300

    feat: modules imported in different ways

--- modulos/modulos.ts
+++ modulos/modulos.ts
@@ -1,6 +1,9 @@
-import { areaRetangulo } from "./retangulo";
-import { areaCircunferencia } from "./circunferencia";
+// import { areaRetangulo } from "./retangulo";
+// import { areaCircunferencia } from "./circunferencia";
+
+import retangulo from "./retangulo";
+import { areaCircunferencia as circ } from "./circunferencia";
 
 console.log("Módulo carregado...");
-console.log(areaRetangulo(7, 8));
-console.log(areaCircunferencia(2));
+console.log(retangulo(7, 8));
+console.log(circ(2));
