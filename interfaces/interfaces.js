4dc62bc44196377415afc9a5391dbe1b7881eb74
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-23 21:21:04 -0300

    feat: methods in interface

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -3,11 +3,18 @@ function saudarComOla(pessoa) {
     console.log(`Olá ${pessoa.nome}`);
 }
 function mudarNome(pessoa) {
-    pessoa.nome = "Maria Eduarda";
+    pessoa.nome = "Luke";
 }
-const pessoa = { nome: "Felipe", idade: 21 };
+const pessoa = {
+    nome: "Felipe",
+    idade: 21,
+    saudar(sobrenome) {
+        console.log(`Olá, meu nome é ${this.nome} ${sobrenome}`);
+    },
+};
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
-saudarComOla({ nome: "Teste", idade: 21, altura: 1.82 });
+// saudarComOla({ nome: "Teste", idade: 21, altura: 1.82 });
+pessoa.saudar("Scãowalker");
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,7 +1,8 @@
 interface Humano {
-    nome: string;
-    idade?: number;
-    [prop: string]: any;
+	nome: string;
+	idade?: number;
+	[prop: string]: any;
+	saudar(sobrenome: string): void;
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -9,12 +10,19 @@ function saudarComOla(pessoa: Humano) {
 }
 
 function mudarNome(pessoa: Humano) {
-	pessoa.nome = "Maria Eduarda";
+	pessoa.nome = "Luke";
 }
 
-const pessoa: Humano = { nome: "Felipe", idade: 21 };
+const pessoa: Humano = {
+	nome: "Felipe",
+	idade: 21,
+	saudar(sobrenome: string) {
+		console.log(`Olá, meu nome é ${this.nome} ${sobrenome}`);
+	},
+};
 
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
-saudarComOla({  nome: "Teste", idade: 21, altura: 1.82 });
+// saudarComOla({ nome: "Teste", idade: 21, altura: 1.82 });
+pessoa.saudar("Scãowalker");
