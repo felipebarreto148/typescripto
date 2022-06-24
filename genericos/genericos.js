e523b05c42e52ce1663a18727ce124f9300a26b4
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-24 19:08:04 -0300

    feat: init generics examples

---
+++ genericos/genericos.js
@@ -0,0 +1,8 @@
+"use strict";
+function echo(objeto) {
+    return objeto;
+}
+console.log(echo("Felipe").length);
+console.log(echo(27));
+console.log(echo({ nome: 'Felipe', idade: 21 }));
+//# sourceMappingURL=genericos.js.map
---
+++ genericos/genericos.ts
@@ -0,0 +1,7 @@
+function echo(objeto: any) {
+    return objeto
+}
+
+console.log(echo("Felipe").length);
+console.log(echo(27));
+console.log(echo({ nome: 'Felipe', idade: 21 }));
