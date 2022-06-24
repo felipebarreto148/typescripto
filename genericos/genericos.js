08f0e06d4618df8240d6141396e65b3b3078e388
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-24 19:12:59 -0300

    feat: Using generics

--- genericos/genericos.js
+++ genericos/genericos.js
@@ -4,5 +4,12 @@ function echo(objeto) {
 }
 console.log(echo("Felipe").length);
 console.log(echo(27));
-console.log(echo({ nome: 'Felipe', idade: 21 }));
+console.log(echo({ nome: "Felipe", idade: 21 }));
+// Usando Gerenics
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado("Felipe").length);
+console.log(echoMelhorado(27));
+console.log(echoMelhorado({ nome: "Felipe", idade: 21 }));
 //# sourceMappingURL=genericos.js.map
--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -1,7 +1,16 @@
 function echo(objeto: any) {
-    return objeto
+	return objeto;
 }
 
 console.log(echo("Felipe").length);
 console.log(echo(27));
-console.log(echo({ nome: 'Felipe', idade: 21 }));
+console.log(echo({ nome: "Felipe", idade: 21 }));
+
+// Usando Gerenics
+function echoMelhorado<TIPO>(objeto: TIPO): TIPO {
+	return objeto;
+}
+
+console.log(echoMelhorado("Felipe").length);
+console.log(echoMelhorado<number>(27));
+console.log(echoMelhorado({ nome: "Felipe", idade: 21 }));
