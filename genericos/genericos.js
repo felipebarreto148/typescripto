79fd542a042d265d06584e0ce3c3f23c7c1c9197
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-27 19:50:24 -0300

    feat: class with generics

--- genericos/genericos.js
+++ genericos/genericos.js
@@ -36,4 +36,36 @@ imprimir([
 ]);
 const chamarEcho = echoMelhorado;
 console.log(chamarEcho("Alguma coisa"));
+// Class com Generics
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+}
+// console.log(new OperacaoBinaria('Bom', 'Dia').executar());
+// console.log(new OperacaoBinaria(3, 7).executar());
+// console.log(new OperacaoBinaria(3, 'Opa').executar());
+class SomaBinaria extends OperacaoBinaria {
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log(new SomaBinaria(3, 7).executar());
+class DiferencaEntreDatas extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/${dia}/${ano}`).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+}
+const d1 = new Data(28, 3, 2022);
+const d2 = new Data(27, 6, 2022);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
 //# sourceMappingURL=genericos.js.map
--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -41,8 +41,46 @@ imprimir<Aluno>([
 	{ nome: "Luke", idade: 0.2 },
 ]);
 
-
 // Tipo Genérico
-type Echo = <T>(data: T) => T
-const chamarEcho: Echo = echoMelhorado
+type Echo = <T>(data: T) => T;
+const chamarEcho: Echo = echoMelhorado;
 console.log(chamarEcho<string>("Alguma coisa"));
+
+// Class com Generics
+abstract class OperacaoBinaria<T, R> {
+	constructor(public operando1: T, public operando2: T) {}
+
+	abstract executar(): R;
+}
+
+// console.log(new OperacaoBinaria('Bom', 'Dia').executar());
+// console.log(new OperacaoBinaria(3, 7).executar());
+// console.log(new OperacaoBinaria(3, 'Opa').executar());
+
+class SomaBinaria extends OperacaoBinaria<number, number> {
+	executar(): number {
+		return this.operando1 + this.operando2;
+	}
+}
+
+console.log(new SomaBinaria(3, 7).executar());
+
+class DiferencaEntreDatas extends OperacaoBinaria<Data, string> {
+	getTime(data: Data): number {
+		let { dia, mes, ano } = data;
+		return new Date(`${mes}/${dia}/${ano}`).getTime();
+	}
+
+	executar(): string {
+		const t1 = this.getTime(this.operando1);
+		const t2 = this.getTime(this.operando2);
+		const diferenca = Math.abs(t1 - t2);
+		const dia = 1000 * 60 * 60 * 24;
+		return `${Math.ceil(diferenca / dia)} dia(s)`;
+	}
+}
+
+const d1 = new Data(28, 3, 2022);
+const d2 = new Data(27, 6, 2022);
+
+console.log(new DiferencaEntreDatas(d1, d2).executar());
