58695320e561ef11fb5a2436f119de8bdb373854
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-27 21:25:21 -0300

    feat: init decorator module

---
+++ decorators/decorators.ts
@@ -0,0 +1,12 @@
+@logarClasse
+class Eletrodomestico {
+    constructor() {
+        console.log('novo...');
+        
+    }
+}
+
+function logarClasse(constructor: Function) {
+    console.log(constructor);
+    
+}
