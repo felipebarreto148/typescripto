74c5846df86ab66458e53fe9faeb01237bc25256
Felipe Barreto <felipebarreto148@gmail.com>
2022-07-04 16:46:32 -0300

    feat: decorators created

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -5,15 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+// // @logarClasse
+// @decorator("Teste", 123)
 let Eletrodomestico = class Eletrodomestico {
     constructor() {
-        console.log('novo...');
+        console.log("novo...");
     }
 };
 Eletrodomestico = __decorate([
-    logarClasse
+    logarClasseSe(true)
 ], Eletrodomestico);
 function logarClasse(constructor) {
     console.log(constructor);
+}
+function decoratorVazio(_) { }
+function logarClasseSe(valor) {
+    return valor ? logarClasse : decoratorVazio;
+}
+function decorator(a, b) {
+    return function (constructor) {
+        console.log(a, b);
+    };
 }
 //# sourceMappingURL=decorators.js.map
--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,12 +1,24 @@
-@logarClasse
+// // @logarClasse
+// @decorator("Teste", 123)
+@logarClasseSe(true)
 class Eletrodomestico {
-    constructor() {
-        console.log('novo...');
-        
-    }
+	constructor() {
+		console.log("novo...");
+	}
 }
 
 function logarClasse(constructor: Function) {
-    console.log(constructor);
-    
+	console.log(constructor);
+}
+
+function decoratorVazio(_: Function) {}
+
+function logarClasseSe(valor: boolean) {
+	return valor ? logarClasse : decoratorVazio;
+}
+
+function decorator(a: string, b: number) {
+	return function (constructor: Function): void {
+		console.log(a, b);
+	};
 }
